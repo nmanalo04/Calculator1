9865bc9c42adb3571e3d9462d92d179d45fa7ae2
Nikaya Manalo <ayamanalo04@gmail.com>
2025-03-18 14:53:49 -0700

    add initial

---
+++ src/CalculatorTest.java
@@ -0,0 +1,36 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void add() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void subtract() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void divide() {
+    }
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testAdd() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testSubtract() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testDivide() {
+    }
+}
