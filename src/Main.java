928138b2fc96bb63c4e1373c78777b9d938904f1
Nikaya Manalo <ayamanalo04@gmail.com>
2025-03-18 14:27:22 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+//Nikaya Manalo
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
+        int sum = calculator.add(5, 3);
+        System.out.println("5 + 3 = " + sum);
+
+    }
+}
