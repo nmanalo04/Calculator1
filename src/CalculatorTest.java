e93caf0e8d7dab24c94d36504255b13ede2f2ba4
Nikaya Manalo <ayamanalo04@gmail.com>
2025-03-18 15:14:25 -0700

    Initial commit with package

--- src/CalculatorTest.java
+++ src/CalculatorTest.java
@@ -1,36 +1,44 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
 class CalculatorTest {
 
-    @org.junit.jupiter.api.BeforeEach
+    Calculator calculator;
+    @BeforeEach
     void setUp() {
+        calculator = new Calculator();
     }
 
-    @org.junit.jupiter.api.Test
-    void add() {
-    }
 
-    @org.junit.jupiter.api.Test
-    void subtract() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void divide() {
-    }
-
-    @org.junit.jupiter.api.BeforeEach
-    void setUp() {
-    }
-
-    @org.junit.jupiter.api.Test
+    @Test
+    @DisplayName("Test For Simple Addition")
     void testAdd() {
+        assertEquals(3, calculator.add(1, 2), "Adds 1 and 2");
+        assertEquals(-2, calculator.add(-4, 2), "Adds a negative numbers");
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
+    @DisplayName("Test For Simple Subtraction")
     void testSubtract() {
+        assertEquals(2, calculator.subtract(4, 2), "4 minus 2");
+        assertEquals(4, calculator.subtract(5, 1), "5 minus 1");
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
+    @DisplayName("Test For Simple Division")
     void testDivide() {
+        assertEquals(2, calculator.divide(6, 3), "4 divide 2");
+        assertEquals(5, calculator.divide(5, 1), "5 divide 1");
     }
-}
+
+    @Test
+    @DisplayName("Division by zero should throw an ArithmeticException")
+    void testDivideByZero() {
+        Exception exception = assertThrows(ArithmeticException.class, () -> calculator.divide(5, 0));
+        assertEquals("Cannot divide by zero", exception.getMessage());
+    }
+
+
+}//end CalculatorTest
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-//nikaya m
 public class Main {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
